d2f3c2291cdd705c64957ea6188998c597bba046
dineshkasi747 <dineshkasi000@gmail.com>
2025-11-01 20:11:45 +0530

    db check error

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,6 +3,9 @@ import {ENV} from "./env.js";
 
 export const ConnectDB = async () => {
     try {
+        if(!ENV.DB_URL){
+            throw new error("no db is found to connect")
+        }
         const conn = await mongoose.connect(ENV.DB_URL);
         console.log("connected to db",conn.connection.host)
     } catch (error) {
