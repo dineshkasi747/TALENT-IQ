9267a1c49e75c67db241108fe80d85bc14e8efd4
dineshkasi747 <dineshkasi000@gmail.com>
2025-11-01 16:36:50 +0530

    deployement

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,23 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import path from "path";
 
 const app = express();
+const __dirname = path.resolve();
 
-app.get('/',(req,res)=>{
+console.log(ENV.NODE_ENV)
+
+app.get('/hello',(req,res)=>{
     res.status(200).json({message:"hii there"})
 });
+
+if(ENV.NODE_ENV=="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get('/{*any}',(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+};
 
 
 app.listen(ENV.PORT,()=>{
