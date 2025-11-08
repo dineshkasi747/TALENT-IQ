a57e66c8abcbab91ab7734905b0af27ac456fc3d
dineshkasi747 <dineshkasi000@gmail.com>
2025-11-08 09:36:30 +0530

    connectdb

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -9,7 +9,7 @@ const syncUser = inngest.createFunction(
   { id: "sync-user" },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    await connectDBnectDB();
+    await connectDB();
 
     const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
